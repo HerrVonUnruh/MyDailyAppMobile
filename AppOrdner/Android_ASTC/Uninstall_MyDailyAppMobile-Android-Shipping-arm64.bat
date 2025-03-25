df291ab70919afb73f8b0730da2ee8db1f679a3a
HerrVonUnruh <118765996+HerrVonUnruh@users.noreply.github.com>
2025-03-25 12:10:58 +0100

    AppVorbereitetFragezeichen

---
+++ AppOrdner/Android_ASTC/Uninstall_MyDailyAppMobile-Android-Shipping-arm64.bat
@@ -0,0 +1,19 @@
+setlocal
+if NOT "%UE_SDKS_ROOT%"=="" (call %UE_SDKS_ROOT%\HostWin64\Android\SetupEnvironmentVars.bat)
+set ANDROIDHOME=%ANDROID_HOME%
+if "%ANDROIDHOME%"=="" set ANDROIDHOME=C:/Users/robin/AppData/Local/Android/Sdk
+set ADB=%ANDROIDHOME%\platform-tools\adb.exe
+set DEVICE=
+if not "%1"=="" set DEVICE=-s %1
+for /f "delims=" %%A in ('%ADB% %DEVICE% shell "echo $EXTERNAL_STORAGE"') do @set STORAGE=%%A
+@echo.
+@echo Uninstalling existing application. Failures here can almost always be ignored.
+%ADB% %DEVICE% uninstall com.mydailyappmobile
+@echo.
+echo Removing old data. Failures here are usually fine - indicating the files were not on the device.
+%ADB% %DEVICE% shell rm -r %STORAGE%/UnrealGame/MyDailyAppMobile
+%ADB% %DEVICE% shell rm -r %STORAGE%/UnrealGame/UECommandLine.txt
+%ADB% %DEVICE% shell rm -r %STORAGE%/obb/com.mydailyappmobile
+%ADB% %DEVICE% shell rm -r %STORAGE%/Android/obb/com.mydailyappmobile
+@echo.
+@echo Uninstall completed
